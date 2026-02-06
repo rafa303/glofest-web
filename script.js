ab0ce87c519e3ec883a8680734202de8ddd1ce6d
rafa303 <57848247+rafa303@users.noreply.github.com>
2026-02-07 01:33:36 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,24 +1,5 @@
-function scrollToContent() {
-  document.querySelector(".content").scrollIntoView({
+function scrollToInfo() {
+  document.getElementById("info").scrollIntoView({
     behavior: "smooth"
   });
 }
-
-/* efek muncul saat scroll */
-const cards = document.querySelectorAll(".card");
-
-window.addEventListener("scroll", () => {
-  cards.forEach(card => {
-    const rect = card.getBoundingClientRect();
-    if (rect.top < window.innerHeight - 100) {
-      card.style.opacity = 1;
-      card.style.transform = "translateY(0)";
-    }
-  });
-});
-
-/* initial state animation */
-cards.forEach(card => {
-  card.style.opacity = 0;
-  card.style.transform = "translateY(40px)";
-});
